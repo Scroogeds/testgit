187d61913669c0ca5c80c5eea197934668d87afc
YoonA4 <1160713762@qq.com>
2023-02-07 13:59:53 +0800

    ceshi

---
+++ Hello.java
@@ -0,0 +1,14 @@
+/**
+ * <p>Title: Hello</p >
+ * <p>Description: </p >
+ *
+ * @author YoonA4
+ * @version 1.0.0
+ * @since 2023/2/7 13:59
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("ceshi");
+    }
+}
